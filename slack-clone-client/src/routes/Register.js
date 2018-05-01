d4b82f94d8a82100019a13743475cbbfd4e7f312
Tien Pham <r3dsky02@yahoo.com.vn>
2018-05-01 19:30:16 +0300

    [client] Handling Register Form Errors in React Using Semantic UI and GraphQL

--- slack-clone-client/src/routes/Register.js
+++ slack-clone-client/src/routes/Register.js
@@ -1,49 +1,101 @@
-import React, { Component } from 'react';
-import { Button, Input, Container, Header } from 'semantic-ui-react';
+import React from 'react';
+import { Message, Button, Input, Container, Header } from 'semantic-ui-react';
 import { gql, graphql } from 'react-apollo';
 
-class Register extends Component {
+class Register extends React.Component {
   state = {
     username: '',
+    usernameError: '',
     email: '',
-    password: ''
-  };
-
-  onChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
+    emailError: '',
+    password: '',
+    passwordError: ''
   };
 
   onSubmit = async () => {
-    const response = await this.props.mutate({
-      variables: this.state
+    this.setState({
+      usernameError: '',
+      emailError: '',
+      passwordError: ''
     });
+
+    const { username, email, password } = this.state;
+    const response = await this.props.mutate({
+      variables: { username, email, password }
+    });
+
+    const { ok, errors } = response.data.register;
+
+    if (ok) {
+      this.props.history.push('/');
+    } else {
+      const err = {};
+      errors.forEach(({ path, message }) => {
+        // err['passwordError'] = 'too long..';
+        err[`${path}Error`] = message;
+      });
+
+      this.setState(err);
+    }
 
     console.log(response);
   };
 
+  onChange = e => {
+    const { name, value } = e.target;
+    // name = "email";
+    this.setState({ [name]: value });
+  };
+
   render() {
-    const { username, email, password } = this.state;
+    const { username, email, password, usernameError, emailError, passwordError } = this.state;
+
+    const errorList = [];
+
+    if (usernameError) {
+      errorList.push(usernameError);
+    }
+
+    if (emailError) {
+      errorList.push(emailError);
+    }
+
+    if (passwordError) {
+      errorList.push(passwordError);
+    }
+
     return (
       <Container text>
         <Header as="h2">Register</Header>
         <Input
+          error={!!usernameError}
           name="username"
           onChange={this.onChange}
           value={username}
           placeholder="Username"
           fluid
         />
-        <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
         <Input
+          error={!!emailError}
+          name="email"
+          onChange={this.onChange}
+          value={email}
+          placeholder="Email"
+          fluid
+        />
+        <Input
+          error={!!passwordError}
           name="password"
           onChange={this.onChange}
           value={password}
           type="password"
-          placeholder="password"
+          placeholder="Password"
           fluid
         />
         <Button onClick={this.onSubmit}>Submit</Button>
+        {usernameError || emailError || passwordError ? (
+          <Message error header="There was some errors with your submission" list={errorList} />
+        ) : null}
       </Container>
     );
   }

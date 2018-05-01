da9f5f8d2e4550dbf9e74f0bf9b2807865ce8ba4
Tien Pham <r3dsky02@yahoo.com.vn>
2018-05-01 15:38:01 +0300

    [client] Setting Up React with GraphQL

--- slack-clone-client/src/index.js
+++ slack-clone-client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Routes from './routes';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:8081/graphql',
+});
+
+const client = new ApolloClient({
+  networkInterface,
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById('root'));

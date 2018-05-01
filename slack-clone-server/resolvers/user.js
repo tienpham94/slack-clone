338f97fdfbd74bb21a7d61a16b0bccbe22753752
Tien Pham <r3dsky02@yahoo.com.vn>
2018-05-01 19:18:44 +0300

    [server] correct password validation

--- slack-clone-server/resolvers/user.js
+++ slack-clone-server/resolvers/user.js
@@ -17,8 +17,21 @@ export default {
   Mutation: {
     register: async (parent, { password, ...otherArgs }, { models }) => {
       try {
+        if (password.length < 5 || password.length > 100) {
+          return {
+            ok: false,
+            errors: [
+              {
+                path: 'password',
+                message: 'The password needs to be between 5 and 100 characters long'
+              }
+            ]
+          };
+        }
+
         const hashedPassword = await bcrypt.hash(password, 12);
         const user = await models.User.create({ ...otherArgs, password: hashedPassword });
+
         return {
           ok: true,
           user

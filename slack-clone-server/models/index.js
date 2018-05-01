c3f95502a409d5ada313663680c2d230a72af4fc
Tien Pham <r3dsky02@yahoo.com.vn>
2018-05-01 13:54:32 +0300

    [server] PostgreSQL Naming Conventions

--- slack-clone-server/models/index.js
+++ slack-clone-server/models/index.js
@@ -2,7 +2,7 @@ import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize('slack', 'postgres', 'postgres', {
   dialect: 'postgres',
-  underscore: true
+  underscored: true
 });
 
 const models = {

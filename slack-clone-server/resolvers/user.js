d2b10349a7c7fa722bc0334f45d0fb62cb456ac3
Tien Pham <r3dsky02@yahoo.com.vn>
2018-05-01 19:14:09 +0300

    [server] Nicer err message for username and pass

--- slack-clone-server/resolvers/user.js
+++ slack-clone-server/resolvers/user.js
@@ -1,19 +1,34 @@
 import bcrypt from 'bcrypt';
+import _ from 'lodash';
+
+const formatErrors = (e, models) => {
+  if (e instanceof models.sequelize.ValidationError) {
+    //  _.pick({a: 1, b: 2}, 'a') => {a: 1}
+    return e.errors.map(x => _.pick(x, ['path', 'message']));
+  }
+  return [{ path: 'name', message: 'something went wrong' }];
+};
 
 export default {
   Query: {
     getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
-    allUsers: (parent, args, { models }) => models.User.findAll(),
+    allUsers: (parent, args, { models }) => models.User.findAll()
   },
   Mutation: {
     register: async (parent, { password, ...otherArgs }, { models }) => {
       try {
         const hashedPassword = await bcrypt.hash(password, 12);
-        await models.User.create({ ...otherArgs, password: hashedPassword });
-        return true;
+        const user = await models.User.create({ ...otherArgs, password: hashedPassword });
+        return {
+          ok: true,
+          user
+        };
       } catch (err) {
-        return false;
+        return {
+          ok: false,
+          errors: formatErrors(err, models)
+        };
       }
-    },
-  },
+    }
+  }
 };
